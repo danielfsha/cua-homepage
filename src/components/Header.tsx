a8785f2d52c8f847e6e27026589517d7f59a999e
Daniel <developedbydanielfisseha@gmail.com>
2025-04-23 01:44:52 -0700

    remoevd unused variable form header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import axios from "axios";
 import { Button } from "@/components/ui/button";
 
